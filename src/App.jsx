8ac19dcf8f81b694dd858c1e44e9d3d4fc9b5688
mubeen2005 <mubinshaikj666@gmail.com>
2025-07-11 07:36:39 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,114 @@
+import React, { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+const UC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const LC = "abcdefghijklmnopqrstuvwxyz"
+const N = "0123456789"
+const SY = "~!@#$$%^&*()_+"
+
+const App = () => {
+
+  let [passwordLen, setPasswordlen] = useState(5)
+  let [uppercase, setUppercase] = useState(false)
+  let [lowercase, setLowercase] = useState(false)
+  let [number, setNumber] = useState(false)
+  let [Symbols, setSymbols] = useState(false)
+  let [fpass, setFpass] = useState("")
+
+  const copy = () => {
+    navigator.clipboard.writeText(fpass)
+    toast("Successfully Copied")
+  }
+
+  const notify = () => {
+    toast("Please Select atleast one Box")
+  }
+
+
+
+
+
+  let createPassword = () => {
+    let finalPassword = ""
+    let charSet = ""
+    if (uppercase | lowercase | number | Symbols) {
+      if (uppercase) charSet = charSet + UC;
+      if (lowercase) charSet = charSet + LC;
+      if (number) charSet = charSet + N;
+      if (Symbols) charSet = charSet + SY;
+      for (let i = 0; i < passwordLen; i++) {
+        finalPassword = finalPassword + charSet.charAt(Math.floor(Math.random() * charSet.length))
+      }
+
+      setFpass(finalPassword)
+    }
+    else {
+      notify()
+    }
+  }
+
+
+  return (
+    <>
+      <div className='bg-[#1c1c1c] w-screen flex items-center justify-center h-screen text-white'>
+        <div className='w-[35%] min-h-[50%] bg-transparent border-2 border-red-500 rounded-xl p-5'>
+          <h1 className='text-3xl font-bold text-center'>Password Generator</h1>
+          <div className='flex mt-5 gap-5'>
+            <input value={fpass} type="text" className='font-bold  px-2 py-2 bg-transparent border-2 border-red-500  rounded w-[80%]  outline-none' />
+            <button onClick={copy} className='bg-red-500 px-5  font-medium active:scale-95 py-2 rounded'>Copy</button>
+          </div>
+
+          <div className='flex items-center justify-between px-2 mt-5'>
+            <h1 className='text-xl font-semibold'>Password Length</h1>
+            <input min={5} max={20} value={passwordLen} onChange={(e) => { setPasswordlen(e.target.value) }} className='w-20 h-10 rounded outline-none text-lg px-2 py-1 bg-transparent border-2 border-red-500' type="number" name="" id="" />
+          </div>
+
+          <div className='flex items-center justify-between px-2 mt-5'>
+            <h1 className='text-xl font-semibold'>Include Uppercase Letters</h1>
+            <input onClick={() => { setUppercase(true) }} className='w-5 h-5 rounded' type="checkbox" name="" id="" />
+          </div>
+
+
+          <div className='flex items-center justify-between px-2 mt-5'>
+            <h1 className='text-xl font-semibold'>Include Lowercase Letters</h1>
+            <input onClick={() => { setLowercase(true) }} className='w-5 h-5 rounded' type="checkbox" name="" id="" />
+          </div>
+
+
+          <div className='flex items-center justify-between px-2 mt-5'>
+            <h1 className='text-xl font-semibold'>Include Numbers</h1>
+            <input onClick={() => { setNumber(true) }} className='w-5 h-5 rounded' type="checkbox" name="" id="" />
+          </div>
+
+
+          <div className='flex items-center justify-between px-2 mt-5'>
+            <h1 className='text-xl font-semibold'>Include Symbols</h1>
+            <input onClick={() => { setSymbols(true) }} className='w-5 h-5 rounded' type="checkbox" name="" id="" />
+          </div>
+
+          <button onClick={createPassword} className='bg-red-500 text-lg active:scale-95 w-full mt-5 py-2 rounded font-semibold'>Generate Password</button>
+        </div>
+
+
+
+      </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+        transition:Bounce
+/>
+    </>
+  )
+}
+
+export default App
